58937054a6766bb55a5c50c7544dc981e07f03e0
Sergey <serq12321@gmail.com>
2021-01-18 13:43:37 +0300

    add Russian

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
 
-        long count_a = 0;
-        long count_A = 0;
+        long count_a_rus = 0;
+        long count_a_eng = 0;
+        long count_A_rus = 0;
+        long count_A_eng = 0;
         long countLine = 0;
 
         char[] currentChars;
@@ -26,10 +28,15 @@ public class Main {
                 currentChars = currentString.toCharArray();
 
                 for (char ch: currentChars) {
+
                     if (ch == 'a') {
-                        count_a++;
+                        count_a_eng++;
                     } else if (ch == 'A') {
-                        count_A++;
+                        count_A_eng++;
+                    } else if (ch == 'а') {
+                        count_a_rus++;
+                    } else if (ch == 'А') {
+                        count_A_rus++;
                     }
                 }
             }
@@ -39,8 +46,10 @@ public class Main {
         }
 
         System.out.println("Количество строк: " + countLine);
-        System.out.println("Количество символов 'a': " + count_a);
-        System.out.println("Количество символов 'a' и 'A': " + (count_a + count_A));
+        System.out.println("Количество символов 'a' английских: " + count_a_eng);
+        System.out.println("Количество символов 'A' английских: "  + count_A_eng);
+        System.out.println("Количество символов 'a' русских: " + count_a_rus);
+        System.out.println("Количество символов 'А' русских: " + count_A_rus);
         System.out.println("Затраченное время в секундах: " + ((double) (System.currentTimeMillis() - time) / 1000));
     }
 }
